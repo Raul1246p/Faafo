f20f2cf7ea968aa9d90b418d4e66bb4a549cd543
Raul1246p <raulchis56@gmail.com>
2026-02-11 11:23:36 +0200

    fixed a bug in programming_humor.py

--- programming_humor.py
+++ programming_humor.py
@@ -1,14 +1,12 @@
 def main():
     hello_world("print")
 
-    name = input("Name? ")
-    hello_world(name)
-
-
-
 
 def hello_world(name="print"):
     if name == "print":
+        print("Hello, World!")
+
+    elif name == "print":
         print("Hello, World!")
         
     else:
